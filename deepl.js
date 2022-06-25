fdb1d548927fff7e5f01761720d7c42752cd654e
MuttC <65762878+muttc@users.noreply.github.com>
2022-06-25 02:54:02 +0200

    Delete deepl.js

--- deepl.js
+++
@@ -1,1 +0,0 @@
-javascript:(function translate(){function se(d) {return d.selection ? d.selection.createRange().text : d.getSelection() }s = se(document);for (i=0; i<frames.length && !s; i++)s = se(frames[i].document);if (!s || s=='') s = prompt('Enter text to translate with DeepL','');open('https://www.deepl.com/translator#../en/'%20+%20(s%20?%20encodeURIComponent(s)%20:%20'')).focus();})();
